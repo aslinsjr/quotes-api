c4950863a97aea1c03c8e9ecc8253090722518f4
aslinsjr <aslinsjr@gmail.com>
2024-03-09 07:06:10 -0300

    .

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Quotes = require('./models/Quotes')
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-)
-app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.json({ message: "Oi Express!" });
+const express = require('express');
+const app = express();
+const quotesJSONArray = require('./quotesSource.json');
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
+app.get('/quotesList',(req, res) => {
+    res.send({ 'quotesList': quotesJSONArray });
+});
 
-mongoose
-  .connect(
-    'mongodb+srv://aslinsjr:23061990@cluster0.pke4fyg.mongodb.net/?retryWrites=true&w=majority',
-  )
-  .then(() => {
-    console.log('Conectou ao banco!')
-    app.listen(3000)
-  })
-  .catch((err) => console.log(err))
+app.get('/randomQuote',(req, res)=>{
+    const randomNumber = Math.floor(Math.random() * 21);
+    const quoteItem = quotesJSONArray[randomNumber]; 
+    res.send(quoteItem);
+});
 
-
-app.post('/quotes', async (req, res) => {
-    const { quote } = req.body
-    const quotes = {
-      quote
-    }
-    try {
-      await Quotes.create(quotes)
-      res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' })
-    } catch (error) {
-      res.status(500).json({ erro: error })
-    }
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
-
-app.get('/quotes', async (req, res) => {
-    try {
-      const quote = await Quotes.find()
-      res.status(200).json(quote)
-    } catch (error) {
-      res.status(500).json({ erro: error })
-    }
-  })
 
   
--- models/Quotes.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const Quotes = mongoose.model('Quotes', {
-  quote: String
-})
-module.exports = Quotes
