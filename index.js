63b76e29d0f13344a72c91643cf93f47f45a42e6
Alexandre Lins <130375942+aslinsjr@users.noreply.github.com>
2024-03-09 07:43:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,12 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
   
